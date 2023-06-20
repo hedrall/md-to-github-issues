1e5a31c1e188597b45b6e25b6bdc568714ad9dc0
hedrall <masaharu73@gmail.com>
2023-06-20 09:10:06 +0900

    fix build

--- build.ts
+++ build.ts
@@ -1,16 +1,6 @@
 import {build} from 'esbuild';
 import * as path from "path";
 
-build({
-    target: 'es2015',
-    entryPoints: [path.resolve(__dirname, './src/index.ts')],
-    outfile: path.resolve(__dirname, './dist/index.js'),
-    minify: false,
-    platform: 'node',
-
-    tsconfig: path.resolve(__dirname, './tsconfig.json'),
-})
-
 // for live demo
 build({
     target: 'es2015',
